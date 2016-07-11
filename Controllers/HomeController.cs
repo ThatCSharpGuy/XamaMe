d05e8179426e4a6157aa38d43e7bc6801e6d3e6e
Antonio Feregrino <hs_mac@MacBook-Pro-de-Antonio.local>
2016-07-11 07:13:08 -0500

    Code!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace XamaMe.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Runtime()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View();
+		}
+
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(HttpPostedFile photo)
+		{
+			
+			return View("XamaMe");
+		}
+	}
+}
+
